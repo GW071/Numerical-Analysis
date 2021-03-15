3afb436ab671756623b1bfc354a0e584b38f3694
takaou <gaowang071@github.com>
2021-03-15 23:52:58 +0800

    完成了非迭代法

--- linear equation system/echo_Tri.cpp
+++ linear equation system/echo_Tri.cpp
@@ -11,11 +11,11 @@ int main(){
     cout<<"Please enter a(i) of A"<<endl;
     for(int i=0;i<n;i++)
         cin>>a[i];
-    cout<<"Please enter c(n) of A from 1 to n-1"<<endl;
-    for(int i=0;i<n-1;i++)
+    cout<<"Please enter c(n) of A"<<endl;
+    for(int i=0;i<n;i++)
         cin>>c[i];
-    cout<<"Please enter d(n) if A from 2 to n"<<endl;
-    for(int i=1;i<n;i++)
+    cout<<"Please enter d(n) if A"<<endl;
+    for(int i=0;i<n;i++)
         cin>>d[i];
     cout<<"Please enter b"<<endl;
     for(int i=0;i<n;i++)
@@ -28,21 +28,36 @@ int main(){
 }
 int pursue(vector<double>& a,vector<double>& c,vector<double>& d,vector<double>& b,vector<double>& res){
     int n=a.size();
-    vector<double> p(n),r(n),q(n);
-    for(int i=0;i<n;i++){
-        r[i]=d[i];
-    }
+    vector<double> p(n),r(n),q(n),s(n);
     p[0]=a[0];
-    for(int i=0;i<n-1;i++){
+    for(int i=0;i<n-2;i++){
         q[i]=c[i]/p[i];
         p[i+1]=a[i+1]-d[i+1]*q[i];
     }
+    s[0]=d[0]/p[0];
+    for(int i=1;i<n-2;i++){
+        s[i]=-d[i]*s[i-1]/p[i];
+    }
+    s[n-2]=(c[n-2]-d[n-2]*s[n-3])/p[n-2];
+    r[0]=c[n-1];
+    for(int j=1;j<n-2;j++){
+        r[j]=-r[j-1]*q[j-1];
+    }
+    r[n-2]=d[n-1]-r[n-3]*q[n-3];
+    r[n-1]=a[n-1];
+    for(int j=0;j<n-1;j++)
+        r[n-1]-=r[j]*s[j];
     vector<double> y(n);
     y[0]=b[0]/p[0];
-    for(int i=1;i<n;i++)
+    for(int i=1;i<n-1;i++)
         y[i]=(b[i]-d[i]*y[i-1])/p[i];
+    y[n-1]=b[n-1];
+    for(int j=0;j<n-1;j++)
+        y[n-1]-=r[j]*y[j];
+    y[n-1]/=r[n-1];
     res[n-1]=y[n-1];
-    for(int i=n-2;i>=0;i--)
-        res[i]=y[i]-q[i]*res[i+1];
+    res[n-2]=y[n-2]-s[n-2]*res[n-1];
+    for(int i=n-3;i>=0;i--)
+        res[i]=y[i]-q[i]*res[i+1]-s[i]*res[n-1];
     return 1;
 }
